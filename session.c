3d45562d697604e30d5a11e89dd7f31a21ccd394
Bastiaan Teeuwen <bastiaan@mkcl.nl>
2018-03-10 11:40:41 +0100

    partially implement pin change mechanism

--- session.c
+++ session.c
@@ -143,7 +143,38 @@ static int pin_update(char **buf)
 	free(*buf);
 	*buf = NULL;
 
-	/* TODO */
+	/* Prepare the query */
+	_q = "SELECT `iban`, `type`, `balance` FROM `accounts` "
+		"WHERE `user_id` = %u ORDER BY `type`";
+	if (!(q = malloc(snprintf(NULL, 0, _q, tok->user_id) + 1)))
+		goto err;
+	sprintf(q, _q, tok->user_id);
+
+	/* Run it */
+	if (mysql_query(sql, q))
+		goto err;
+	if (!(res = mysql_store_result(sql)))
+		goto err;
+
+	/* Allocate and fill the array */
+	n = mysql_num_rows(res);
+	if (!(a = malloc(n * sizeof(struct kbp_reply_account))))
+		goto err;
+	*buf = (char *) a;
+
+	while ((row = mysql_fetch_row(res))) {
+		strncpy((*a).iban, row[0], KBP_IBAN_MAX);
+		(*a).type = strtol(row[1], NULL, 10);
+		(*a).balance = strtoll(row[2], NULL, 10);
+		a++;
+	}
+
+	free(q);
+	mysql_free_result(res);
+	/*
+	 * TODO
+	 * - 
+	 */
 
 	return -1;
 }
@@ -151,23 +182,24 @@ static int pin_update(char **buf)
 /* This entire function is a fucking mess */
 static int login(MYSQL *sql, struct token *tok, char **buf)
 {
-	struct kbp_request_login l;
+	char pin[KBP_PIN_MAX + 1];
 	char *_q, *q = NULL;
 	MYSQL_RES *res = NULL;
 	MYSQL_ROW row;
 	int _res = KBP_L_GRANTED;
 	char *r;
 
-	memcpy(&l, *buf, sizeof(l));
+	memcpy(&pin, *buf, sizeof(KBP_PIN_MAX + 1));
 	free(*buf);
 	*buf = NULL;
 
 	/* Prepare the query */
-	_q = "SELECT `pin`, `attempts` FROM `cards` WHERE `user_id` = %u AND "
-			"`card_id` = %u";
-	if (!(q = malloc(snprintf(NULL, 0, _q, l.user_id, l.card_id) + 1)))
+		_q = "UPDATE `` SET `pin` = '%s' WHERE "
+				"`user_id` = %u AND `card_id` = %u";
+	if (!(q = malloc(snprintf(NULL, 0, _q,
+			tok->user_id, tok->card_id) + 1)))
 		goto err;
-	sprintf(q, _q, l.user_id, l.card_id);
+	sprintf(q, _q, tok->user_id, tok->card_id);
 
 	/* Run it */
 	if (mysql_query(sql, q))
